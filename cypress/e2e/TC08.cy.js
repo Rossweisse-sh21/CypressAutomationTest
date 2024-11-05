42413325dbb7734286598c8bd51e05e9206504e7
Rossweisse-sh21 <85684171+Rossweisse-sh21@users.noreply.github.com>
2024-11-05 22:59:54 +0700

    Update TC08.cy.js

--- cypress/e2e/TC08.cy.js
+++ cypress/e2e/TC08.cy.js
@@ -1,12 +1,12 @@
 describe("Sort Products by Price (High to Low) on SauceDemo", () => {
   it("Should sort products in descending order by price", () => {
-    // Given I am logged in as "standard_user"
+    // Logged in as "standard_user"
     cy.visit("https://www.saucedemo.com/");
     cy.get("#user-name").type("standard_user");
     cy.get("#password").type("secret_sauce");
     cy.get("#login-button").click();
 
-    // Verify we are on the products page
+    // Verify at products page
     cy.url().should("include", "/inventory.html");
     cy.get("#inventory_container").should("be.visible");
 
