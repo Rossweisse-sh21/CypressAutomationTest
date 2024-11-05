bafa7cbf56c77df288e6fac3aab805f5f2d8c6a4
Rossweisse-sh21 <85684171+Rossweisse-sh21@users.noreply.github.com>
2024-11-05 22:29:49 +0700

    Update TC02.cy.js

--- cypress/e2e/TC02.cy.js
+++ cypress/e2e/TC02.cy.js
@@ -1,12 +1,18 @@
 describe("SauceDemo E2E Tests", () => {
   beforeEach(() => {
+    // Open the SauceDemo login page before each test
     cy.visit("https://www.saucedemo.com/");
   });
 
   it("Login with invalid credentials", () => {
+    // Enter an invalid username and password
     cy.get("#user-name").type("invalid_user");
     cy.get("#password").type("wrong_pass");
+
+    // Click the login button
     cy.get("#login-button").click();
+
+    // Verify that an error message is displayed
     cy.get('[data-test="error"]')
       .should("be.visible")
       .and("contain", "Username and password do not match");
