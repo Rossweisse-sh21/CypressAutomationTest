e0ededfdcd1faa02cc61b18ab0f83e7729cbc530
Rossweisse-sh21 <85684171+Rossweisse-sh21@users.noreply.github.com>
2024-11-05 23:00:09 +0700

    Update TC09.cy.js

--- cypress/e2e/TC09.cy.js
+++ cypress/e2e/TC09.cy.js
@@ -1,12 +1,12 @@
 describe("Sort Products by name (Z to A) on SauceDemo", () => {
   it("Should sort products in name order 9Z to A)", () => {
-    // Given I am logged in as "standard_user"
+    // Logged in as "standard_user"
     cy.visit("https://www.saucedemo.com/");
     cy.get("#user-name").type("standard_user");
     cy.get("#password").type("secret_sauce");
     cy.get("#login-button").click();
 
-    // Verify we are on the products page
+    // Verify at products page
     cy.url().should("include", "/inventory.html");
     cy.get("#inventory_container").should("be.visible");
 
