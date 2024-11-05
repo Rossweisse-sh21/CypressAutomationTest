3e3531cc82578e5f10ba1abe1870f2c657d1ff3d
Rossweisse-sh21 <85684171+Rossweisse-sh21@users.noreply.github.com>
2024-11-05 23:00:27 +0700

    Update TC10.cy.js

--- cypress/e2e/TC10.cy.js
+++ cypress/e2e/TC10.cy.js
@@ -1,18 +1,18 @@
 describe("Logout Test on SauceDemo", () => {
   it("Should log out successfully and return to the login page", () => {
-    // Given I am logged in as "standard_user"
+    // Logged in as "standard_user"
     cy.visit("https://www.saucedemo.com/");
     cy.get("#user-name").type("standard_user");
     cy.get("#password").type("secret_sauce");
     cy.get("#login-button").click();
 
-    // When I click on the sidebar menu icon
+    // Click on the sidebar menu icon
     cy.get("#react-burger-menu-btn").click();
 
-    // And I select "Logout"
+    // Select "Logout"
     cy.get("#logout_sidebar_link").click();
 
-    // Then I should be redirected to the login page
+    // Redirected to the login page
     cy.url().should("include", "/");
     cy.get("#login-button").should("be.visible");
   });
