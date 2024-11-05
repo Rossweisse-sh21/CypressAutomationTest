d94dab79cbd022da3665a81a0e8925234cfedc5e
Rossweisse-sh21 <85684171+Rossweisse-sh21@users.noreply.github.com>
2024-11-05 22:59:41 +0700

    Update TC07.cy.js

--- cypress/e2e/TC07.cy.js
+++ cypress/e2e/TC07.cy.js
@@ -1,16 +1,16 @@
 describe("Sort Products by Price (low to high) on SauceDemo", () => {
   it("Should sort products in ascending order by price", () => {
-    // Given I am logged in as "standard_user"
+    // Logged in as "standard_user"
     cy.visit("https://www.saucedemo.com/");
     cy.get("#user-name").type("standard_user");
     cy.get("#password").type("secret_sauce");
     cy.get("#login-button").click();
 
-    // Verify we are on the products page
+    // Verify at products page
     cy.url().should("include", "/inventory.html");
     cy.get("#inventory_container").should("be.visible");
 
     // Select "Price (low to high)" from the sort dropdown
-    cy.get(".product_sort_container").select("lohi"); // or .select('lohi')
+    cy.get(".product_sort_container").select("lohi");
   });
 });
